5a604be9d08c71b3ae62f1be86b79b797b76cf77
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2023-07-18 15:44:02 +0200

    Register all important topics

--- bb_utils/src/rewrite_bag_timestamps.cpp
+++ bb_utils/src/rewrite_bag_timestamps.cpp
@@ -13,25 +13,33 @@
 #include <rosbag2_cpp/writers/sequential_writer.hpp>
 
 #include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <tf2_msgs/msg/tf_message.hpp>
+#include <rosgraph_msgs/msg/clock.hpp>
 
 // To run this type:
 // ros2 run bb_utils rewrite_bag_timestamps /path/to/input.bag /path/to/output.bag
 // Example:
 // ros2 run bb_utils rewrite_bag_timestamps /home/ale/bag_files/static_sensors_only /home/ale/bag_files/static_sensors_only_correct_ts
 
+builtin_interfaces::msg::Time getStamp(std::shared_ptr<rosbag2_storage::SerializedBagMessage> bag_message, std::string topic_type);
+
 // Function that rewrites the timestamps inside the input_bag to match the timestamps inside the messages and publishes a new bag
 void rewriteBagTimestamps(const std::string& input_bag, const std::string& output_bag)
 {
 
-  std::vector<std::string> topics_to_keep = {"/carla/sensors_home/static_rgb_camera/image", 
+  std::vector<std::string> topics_to_keep = {
+                                    "/carla/sensors_home/static_rgb_camera/image", 
                                     "/carla/sensors_home/static_depth_camera/image",
                                     "/carla/sensors_home/static_termic_camera/image",
-                                    "/carla/sensors_home/static_lidar"
+                                    "/carla/sensors_home/static_rgb_camera/camera_info", 
+                                    "/carla/sensors_home/static_depth_camera/camera_info",
+                                    "/carla/sensors_home/static_termic_camera/camera_info",
+                                    "/carla/sensors_home/static_lidar",
+                                    "/tf",
+                                    "/clock"
                                     };
-
-  rclcpp::Serialization<sensor_msgs::msg::Image> image_serializer;
-  rclcpp::Serialization<sensor_msgs::msg::PointCloud2> lidar_serializer;
   
   std::unique_ptr<rosbag2_cpp::readers::SequentialReader> reader;
   std::unique_ptr<rosbag2_cpp::writers::SequentialWriter> writer;
@@ -86,19 +94,9 @@ void rewriteBagTimestamps(const std::string& input_bag, const std::string& outpu
     // Read the next message
     bag_message = reader->read_next();
     topic_type = topic_to_type.at(bag_message->topic_name);
-    rclcpp::SerializedMessage extracted_serialized_msg(*bag_message->serialized_data);
-    if(topic_type == "sensor_msgs/msg/Image"){
-      sensor_msgs::msg::Image extracted_msg;
-      image_serializer.deserialize_message(&extracted_serialized_msg, &extracted_msg);
-      stamp = extracted_msg.header.stamp;
-    }
-    else if(topic_type == "sensor_msgs/msg/PointCloud2"){
-      sensor_msgs::msg::PointCloud2 extracted_msg;
-      lidar_serializer.deserialize_message(&extracted_serialized_msg, &extracted_msg);
-      stamp = extracted_msg.header.stamp;
-    }
 
     // Get the timestamp from the header
+    stamp = getStamp(bag_message, topic_type);
     const int64_t timestamp = int64_t(stamp.sec) * 1000000000 + int64_t(stamp.nanosec);
 
     // std::cout << "stamp: \t\t" << bag_message->time_stamp << std::endl;
@@ -109,7 +107,6 @@ void rewriteBagTimestamps(const std::string& input_bag, const std::string& outpu
 
     // Write the modified message to the output bag
     writer->write(bag_message);
-
   }
 
   std::cout<< "Finished" << std::endl;
@@ -118,6 +115,45 @@ void rewriteBagTimestamps(const std::string& input_bag, const std::string& outpu
   std::chrono::duration<double> duration = end - start_time;
   // Print the duration in seconds
   std::cout << "Execution time: " << duration.count() << " seconds." << std::endl;
+}
+
+builtin_interfaces::msg::Time getStamp(std::shared_ptr<rosbag2_storage::SerializedBagMessage> bag_message, std::string topic_type){
+
+  static rclcpp::Serialization<sensor_msgs::msg::Image> image_serializer;
+  static rclcpp::Serialization<sensor_msgs::msg::CameraInfo> cam_info_serializer;
+  static rclcpp::Serialization<sensor_msgs::msg::PointCloud2> lidar_serializer;
+  static rclcpp::Serialization<tf2_msgs::msg::TFMessage> tf_serializer;
+  static rclcpp::Serialization<rosgraph_msgs::msg::Clock> clock_serializer;
+
+  builtin_interfaces::msg::Time stamp;
+  rclcpp::SerializedMessage extracted_serialized_msg(*bag_message->serialized_data);
+    if(topic_type == "sensor_msgs/msg/Image"){
+      sensor_msgs::msg::Image extracted_msg;
+      image_serializer.deserialize_message(&extracted_serialized_msg, &extracted_msg);
+      stamp = extracted_msg.header.stamp;
+    }
+    else if(topic_type == "sensor_msgs/msg/PointCloud2"){
+      sensor_msgs::msg::PointCloud2 extracted_msg;
+      lidar_serializer.deserialize_message(&extracted_serialized_msg, &extracted_msg);
+      stamp = extracted_msg.header.stamp;
+    }
+    else if(topic_type == "tf2_msgs/msg/TFMessage"){
+      tf2_msgs::msg::TFMessage extracted_msg;
+      tf_serializer.deserialize_message(&extracted_serialized_msg, &extracted_msg);
+      stamp = extracted_msg.transforms[0].header.stamp;
+    }
+    else if(topic_type == "rosgraph_msgs/msg/Clock"){
+      rosgraph_msgs::msg::Clock extracted_msg;
+      clock_serializer.deserialize_message(&extracted_serialized_msg, &extracted_msg);
+      stamp = extracted_msg.clock;
+    }
+    else if(topic_type == "sensor_msgs/msg/CameraInfo"){
+      sensor_msgs::msg::CameraInfo extracted_msg;
+      cam_info_serializer.deserialize_message(&extracted_serialized_msg, &extracted_msg);
+      stamp = extracted_msg.header.stamp;
+    }
+
+  return stamp;
 }
 
 int main(int argc, char* argv[])
