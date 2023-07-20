f9939d2bf7d089b5ded63a4116c3cfa724b58407
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2023-07-20 15:39:24 +0200

    Signal node not used

--- bb_utils/src/bag_recorder.cpp
+++ bb_utils/src/bag_recorder.cpp
@@ -6,6 +6,8 @@
 #include <rosbag2_cpp/writers/sequential_writer.hpp>
 #include <rosbag2_storage/serialized_bag_message.hpp>
 
+// %%%%%%%%%%%% This is not currently used, kept for reference %%%%%%%%%%%%
+
 using std::placeholders::_1;
 
 class BagRecorder : public rclcpp::Node
