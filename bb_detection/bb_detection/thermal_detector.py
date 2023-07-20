dde2c42c235b1ca9481232328be15d8e4b108ac1
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2023-07-20 18:48:27 +0200

    Publish the 3D bboxes (wrong distance from camera)

--- bb_detection/bb_detection/thermal_detector.py
+++ bb_detection/bb_detection/thermal_detector.py
@@ -18,9 +18,6 @@ from geometry_msgs.msg import Quaternion
 # Other
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
-import random
-from ultralytics import YOLO
-from ultralytics.yolo.engine.results import Results
 import numpy as np
 # My files
 from .utility import *
@@ -132,6 +129,40 @@ class ThermalDetector(Node):
                 x, y, w, h = cv2.boundingRect(contour)
                 min_pt = (x, y)
                 max_pt = (x + w, y + h)
+
+                detection = Detection3D()
+
+                center_coordinates = (min_pt[0] + (max_pt[0]-min_pt[0])/2, min_pt[1] + (max_pt[1]-min_pt[1])/2)
+
+                # The depth data is stored inside the cv_dep numpy array -> y are the rows and x are columns
+                # z_value = cv_dep[int(uv_coordinates[1])][int(uv_coordinates[0])]
+                z_value = 1 # TODO: find the z value using trigonometry knowing the angle that the camera has with the ground
+
+                # Project the 3 points
+                detection.bbox.center.position.x, \
+                    detection.bbox.center.position.y, \
+                        detection.bbox.center.position.z  = project_to_3D_space(camera_model, center_coordinates, z_value)
+                
+                min_pt_3d = project_to_3D_space(camera_model, min_pt, z_value)
+                max_pt_3d = project_to_3D_space(camera_model, max_pt, z_value)
+                
+                # Add an orientation to remove the tilt of the camera and place the bbox orizontally
+                detection.bbox.center.orientation = q_to_apply
+
+                # Compute the size of the bounding box looking at the projections of the two vertexes
+                detection.bbox.size.x = float(max_pt_3d[0]-min_pt_3d[0])
+                detection.bbox.size.y = float(max_pt_3d[1]-min_pt_3d[1])
+                detection.bbox.size.z = self.class_to_z_val[class_name]
+
+                # create hypothesis
+                hypothesis = ObjectHypothesisWithPose()
+                hypothesis.id = class_name
+                hypothesis.score = 1.0
+                detection.results.append(hypothesis)
+
+                # append msg
+                detections_msg.detections.append(detection)
+
                 if(self.get_parameter('show_debug').value):
                     cv2.rectangle(cv_image, min_pt, max_pt, bounding_box_color, 2)
                     pos = (min_pt[0] + 5, min_pt[1] + 25)
@@ -139,66 +170,8 @@ class ThermalDetector(Node):
                     cv2.putText(cv_image, class_name, pos, font,
                                 1, bounding_box_color, 1, cv2.LINE_AA)
 
-
-# TODO: Add 3D bbox part
-
-        # for box_data in results.boxes:
-
-        #     detection = Detection3D()
-
-        #     # get label and score
-        #     label = self.yolo.names[int(box_data.cls)]
-        #     score = float(box_data.conf)
-
-        #     # get boxes values
-        #     box = box_data.xywh[0]
-
-        #     uv_coordinates = (box[0], box[1])
-        #     bbox_image_size = (box[2], box[3])
-        #     # The depth data is stored inside the cv_dep numpy array -> y are the rows and x are columns
-        #     z_value = cv_dep[int(uv_coordinates[1])][int(uv_coordinates[0])]
-
-        #     # bottom-left
-        #     min_pt = (int(uv_coordinates[0] - bbox_image_size[0] / 2.0),
-        #                     int(uv_coordinates[1] - bbox_image_size[1] / 2.0))
-        #     # top-right
-        #     max_pt = (int(uv_coordinates[0] + bbox_image_size[0] / 2.0),
-        #                     int(uv_coordinates[1] + bbox_image_size[1] / 2.0))
-
-        #     # Project the 3 points
-        #     detection.bbox.center.position.x, \
-        #         detection.bbox.center.position.y, \
-        #             detection.bbox.center.position.z  = project_to_3D_space(camera_model, uv_coordinates, z_value)
-            
-        #     min_pt_3d = project_to_3D_space(camera_model, min_pt, z_value)
-        #     max_pt_3d = project_to_3D_space(camera_model, max_pt, z_value)
-            
-        #     # Add an orientation to remove the tilt of the camera and place the bbox orizontally
-        #     detection.bbox.center.orientation = q_to_apply
-
-        #     # Compute the size of the bounding box looking at the projections of the two vertexes
-        #     detection.bbox.size.x = float(max_pt_3d[0]-min_pt_3d[0])
-        #     detection.bbox.size.y = float(max_pt_3d[1]-min_pt_3d[1])
-        #     detection.bbox.size.z = self.class_to_z_val[label]
-
-        #     # get track id
-        #     track_id = ""
-        #     if box_data.is_track:
-        #         track_id = str(int(box_data.id))
-
-        #     # create hypothesis
-        #     hypothesis = ObjectHypothesisWithPose()
-        #     hypothesis.id = label
-        #     hypothesis.score = score
-        #     detection.results.append(hypothesis)
-
-            
-                
-            # append msg
-            # detections_msg.detections.append(detection)
-
         # publish detections
-        # self._pub.publish(detections_msg)
+        self._pub.publish(detections_msg)
 
         if(self.get_parameter('show_debug').value):
             # Display image
