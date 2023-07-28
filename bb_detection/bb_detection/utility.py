9f23b4a7a9375a4d8f045b2f5047768a563b0830
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2023-07-28 21:02:44 +0200

    change variable names

--- bb_detection/bb_detection/utility.py
+++ bb_detection/bb_detection/utility.py
@@ -28,18 +28,18 @@ def euler_from_quaternion(q : Quaternion):
 
     t0 = +2.0 * (w * x + y * z)
     t1 = +1.0 - 2.0 * (x * x + y * y)
-    roll_x = math.atan2(t0, t1)
+    roll = math.atan2(t0, t1)
     
     t2 = +2.0 * (w * y - z * x)
     t2 = +1.0 if t2 > +1.0 else t2
     t2 = -1.0 if t2 < -1.0 else t2
-    pitch_y = math.asin(t2)
+    pitch = math.asin(t2)
     
     t3 = +2.0 * (w * z + x * y)
     t4 = +1.0 - 2.0 * (y * y + z * z)
-    yaw_z = math.atan2(t3, t4)
+    yaw = math.atan2(t3, t4)
     
-    return roll_x, pitch_y, yaw_z # in radians
+    return roll, pitch, yaw # in radians
 
 def find_3d_point_on_line(line_vector, z_value):
     '''
