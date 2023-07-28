ee73000eb7aeec929795d69e8046b22f2f94adb5
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2023-07-28 21:02:14 +0200

    Add bev submodule

--- bb_detection/setup.py
+++ bb_detection/setup.py
@@ -7,7 +7,7 @@ package_name = 'bb_detection'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name, package_name + '/bev'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
