b4cc8d85609c5c129ea9eec445efbc3c809b4a47
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2023-08-03 00:00:48 +0200

    Transform all BBox to the same fixed frame

--- bb_tracker/src/bb_tracker.cpp
+++ bb_tracker/src/bb_tracker.cpp
@@ -1,8 +1,15 @@
+// C++
 #include <memory>
-
+// ROS2
 #include <rclcpp/rclcpp.hpp>
+#include <tf2/transform_datatypes.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+// ROS msgs
 #include <vision_msgs/msg/detection3_d_array.hpp>
 #include <vision_msgs/msg/detection3_d.hpp>
+// extra
 #include <opencv2/opencv.hpp>
 #include <bb_tracker/BYTETracker.h>
 
@@ -16,9 +23,12 @@ class BBTracker : public rclcpp::Node
 {
   public:
     BBTracker()
-    : Node("bb_tracker")
+    : Node("bb_tracker"), _tf_buffer(this->get_clock()), _tf_listener(_tf_buffer)
     {
         this->declare_parameter("fps", 30);
+        this->declare_parameter("fixed_frame", "sensors_home");
+
+        _fixed_frame = get_parameter("fixed_frame").as_string();
         int fps = get_parameter("fps").as_int();
 
         _detection = this->create_subscription<vision_msgs::msg::Detection3DArray>(
@@ -31,6 +41,29 @@ class BBTracker : public rclcpp::Node
         _total_ms = 0;
     
         RCLCPP_INFO(this->get_logger(), "Ready to track");
+    }
+
+    //FIXME: try with yolo detector
+    void change_frame(std::shared_ptr<vision_msgs::msg::Detection3DArray> old_message, std::string& new_frame){
+      auto detections = old_message->detections;
+      for(auto& detect : detections){
+        // Transform
+        geometry_msgs::msg::PoseStamped old_pose;
+        old_pose.header = detect.header;
+        old_pose.pose = detect.bbox.center;
+        auto new_pose = _tf_buffer.transform(old_pose, new_frame);
+        geometry_msgs::msg::Vector3Stamped old_size;
+        old_size.header = detect.header;
+        old_size.vector = detect.bbox.size;
+        auto new_size = _tf_buffer.transform(old_size, new_frame);
+        // Assign back
+        detect.header.frame_id = new_frame;
+        detect.bbox.center = new_pose.pose;
+        detect.bbox.size = new_size.vector;
+      }
+
+      old_message->header.frame_id = new_frame;
+      return;
     }
 
   private:
@@ -57,7 +90,7 @@ class BBTracker : public rclcpp::Node
         RCLCPP_INFO(this->get_logger(), "I heard from: '%s'", detections_message->header.frame_id.c_str());
       #endif
       _num_detections++;
-      if (_num_detections % 20 == 0)
+      if (_num_detections % 50 == 0)
       {
           RCLCPP_INFO(this->get_logger(), "Processing frame %d (%d fps)", _num_detections, _num_detections * 1000000 / _total_ms);
           //std::cout << "Processing frame " << _num_detections << " (" << _num_detections * 1000000 / _total_ms << " fps)" << std::endl;
@@ -67,6 +100,10 @@ class BBTracker : public rclcpp::Node
     
       // Decode detections and update the tracker
       auto start = chrono::system_clock::now();
+      // Move all the detections to a common fixed frame
+      if(detections_message->header.frame_id != _fixed_frame){
+        change_frame(detections_message, _fixed_frame);
+      }
       vector<Object> objects;
       // Put detection3d array inside the object structure
       decode_detections(detections_message, objects);
@@ -83,6 +120,7 @@ class BBTracker : public rclcpp::Node
 
       auto out_message = vision_msgs::msg::Detection3DArray();
       out_message.header = detections_message->header;
+      out_message.header.frame_id = _fixed_frame;
       out_message.detections.resize(output_stracks.size());
       for (unsigned int i = 0; i < output_stracks.size(); i++)
       {
@@ -110,6 +148,7 @@ class BBTracker : public rclcpp::Node
         single_det.bbox.center.position.x = minwdh[0] + minwdh[3]/2;
         single_det.bbox.center.position.y = minwdh[1] + minwdh[4]/2;
         single_det.bbox.center.position.z = minwdh[2] + minwdh[5]/2;
+        single_det.bbox.center.orientation.w = 1.0;
         single_det.bbox.size.x =            minwdh[3];
         single_det.bbox.size.y =            minwdh[4];
         single_det.bbox.size.z =            minwdh[5];
@@ -129,16 +168,19 @@ class BBTracker : public rclcpp::Node
       }
 
       _det_publisher->publish(out_message);
-      // TODO: Show Progress
-      // putText(img, format("frame: %d fps: %d num_tracks: %d", _num_detections, _num_detections * 1000000 / _total_ms, output_stracks.size()), 
-      //         Point(0, 30), 0, 0.6, Scalar(0, 0, 255), 2, LINE_AA);
+      // Show Progress
+      std::cout << format("frame: %d fps: %d num_tracks: %lu", _num_detections, _num_detections * 1000000 / _total_ms, output_stracks.size()) << "\r";
+      std::cout.flush();
     }
 
 
   private:
 
+    tf2_ros::Buffer _tf_buffer;
+    tf2_ros::TransformListener _tf_listener;
     rclcpp::Publisher<vision_msgs::msg::Detection3DArray>::SharedPtr _det_publisher;
     rclcpp::Subscription<vision_msgs::msg::Detection3DArray>::SharedPtr _detection;
+    std::string _fixed_frame;
     BYTETracker _tracker;
     int _num_detections;
     int _total_ms;
