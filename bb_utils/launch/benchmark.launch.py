9a280de5e10d2d03527ad01d25d121d6f8e6cfd9
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2023-11-24 11:56:48 +0100

    Remove repetitions in launch

--- bb_utils/launch/benchmark.launch.py
+++ bb_utils/launch/benchmark.launch.py
@@ -8,15 +8,7 @@ from ament_index_python.packages import get_package_share_directory
 
 import sys, os
 
-# set launch arguments
-bag_name = 'static_sensors_only'
-for arg in sys.argv:
-    if arg.startswith("bag_name:="):
-        bag_name = str(arg.split(":=")[1])
-
 def generate_launch_description():
-    bag_folder = os.getenv('BAG_DIR') # set environmental variable BAG_DIR to the current directory where you save the bag files
-    rviz_folder = os.getenv('RVIZ2_DIR') # set environmental variable RVIZ2_DIR to the current directory where you save the rviz2 config
 
     config = os.path.join(
         get_package_share_directory('bb_utils'),
